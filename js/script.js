84a90d20d86955415c95a2a56db73c90af33e18d
Janette Campbell <campbjp@hotmail.com>
2022-10-27 19:32:48 -0400

    feat: added ball shrink and grow on mouse position

--- js/script.js
+++ js/script.js
@@ -8,6 +8,18 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// declare variables
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+const maxRadius = 40;
+const minRadius = 2;
+const velocity = 3;
+let circleArray = [];
+let rectangleArray = [];
+const colorArray = ["#2C3E50", "#E74C3C", "#ECF0F1", "#3498DB", "#2980B9"];
+
 // // draw rectangles on canvas
 // // ctx.fillRect(x, y, width, height)
 // ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
@@ -41,6 +53,11 @@ canvas.height = window.innerHeight;
 //   ctx.stroke();
 // }
 
+/**
+ * ----------------------------------------------
+ *             CIRCLE ANIMATION
+ * ----------------------------------------------
+ */
 // animate arc or circle
 function Circle(x, y, dx, dy, radius, color) {
   this.x = x;
@@ -48,12 +65,13 @@ function Circle(x, y, dx, dy, radius, color) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
-  this.color = color;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+  this.minRadius = radius;
 
   this.draw = function () {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.fillStyle = color;
+    ctx.fillStyle = this.color;
     ctx.fill();
   };
 
@@ -69,23 +87,46 @@ function Circle(x, y, dx, dy, radius, color) {
     this.x += this.dx;
     this.y += this.dy;
 
+    // interactivity
+    // if circle is moused over increase in size, decrease if mouse away
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius++;
+      }
+    } else if (this.radius > this.minRadius) {
+      this.radius--;
+    }
+
     this.draw();
   };
 }
 
-let circleArray = [];
+const initCircle = () => {
+  circleArray = [];
 
-for (let i = 0; i < 100; i++) {
-  const radius = Math.random() * (50 - 5) + 5;
-  const x = Math.random() * (window.innerWidth - radius * 2) + radius;
-  const y = Math.random() * (window.innerHeight - radius * 2) + radius;
-  const dx = (Math.random() - 0.5) * 8;
-  const dy = (Math.random() - 0.5) * 8;
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  const color = "#" + randomColor;
+  for (let i = 0; i < 800; i++) {
+    const radius = Math.random() * (5 - 2) + 2;
+    const x = Math.random() * (window.innerWidth - radius * 2) + radius;
+    const y = Math.random() * (window.innerHeight - radius * 2) + radius;
+    const dx = (Math.random() - 0.5) * velocity;
+    const dy = (Math.random() - 0.5) * velocity;
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const color = "#" + randomColor;
 
-  circleArray.push(new Circle(x, y, dx, dy, radius, color));
-}
+    circleArray.push(new Circle(x, y, dx, dy, radius, color));
+  }
+};
+
+/**
+ * ----------------------------------------------
+ *             RECTANGLE ANIMATION
+ * ----------------------------------------------
+ */
 
 function Rectangle(x, y, wx, hy, dx, dy, color) {
   this.x = x;
@@ -115,24 +156,37 @@ function Rectangle(x, y, wx, hy, dx, dy, color) {
     this.x += this.dx;
     this.y += this.dy;
 
+    // //interactivity
+    // if (
+    //   mouse.x - this.x < 50 &&
+    //   mouse.x - this.x > -50 &&
+    //   mouse.y - this.y < 50 &&
+    //   mouse.y - this.y > -50
+    // ) {
+    //   this.wx++;
+    //   this.hy++;
+    // } else {
+    //   this.wx--;
+    //   this.hy--;
+    // }
     this.draw();
   };
 }
 
-let rectangleArray = [];
+const initRectangle = () => {
+  for (let i = 0; i < 100; i++) {
+    const wx = Math.random() * (75 - 20) + 20;
+    const hy = Math.random() * (75 - 20) + 20;
+    const x = Math.random() * (window.innerWidth - wx * 2) + wx;
+    const y = Math.random() * (window.innerHeight - hy * 2) + hy;
+    const dx = (Math.random() - 0.5) * velocity;
+    const dy = (Math.random() - 0.5) * velocity;
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const color = "#" + randomColor;
 
-for (let i = 0; i < 100; i++) {
-  const wx = Math.random() * (75 - 20) + 20;
-  const hy = Math.random() * (75 - 20) + 20;
-  const x = Math.random() * (window.innerWidth - wx * 2) + wx;
-  const y = Math.random() * (window.innerHeight - hy * 2) + hy;
-  const dx = (Math.random() - 0.5) * 8;
-  const dy = (Math.random() - 0.5) * 8;
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  const color = "#" + randomColor;
-
-  rectangleArray.push(new Rectangle(x, y, wx, hy, dx, dy, color));
-}
+    rectangleArray.push(new Rectangle(x, y, wx, hy, dx, dy, color));
+  }
+};
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -142,9 +196,32 @@ const animate = () => {
     circleArray[i].update();
   }
 
-  for (let i = 0; i < rectangleArray.length; i++) {
-    rectangleArray[i].update();
-  }
+  //   for (let i = 0; i < rectangleArray.length; i++) {
+  //     rectangleArray[i].update();
+  //   }
 };
 
+initCircle();
 animate();
+
+/**
+ * ----------------------------------------------
+ *             INTERACTIVITY
+ * ----------------------------------------------
+ */
+
+const mouseHover = (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+};
+
+const responsiveWindow = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  initCircle();
+};
+
+window.addEventListener("mousemove", mouseHover);
+
+window.addEventListener("resize", responsiveWindow);
